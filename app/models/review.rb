9a5e30c71a4fa2869bc0badcafdaa7aea009e2e3
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-17 01:09:39 +0000

    Added validations to Review model.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,14 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
+
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, presence: true,
+    numericality: {
+      only_integer: true,
+      greater_than: 0,
+      less_than: 6
+    }
+
 end
