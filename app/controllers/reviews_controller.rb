71911d9b9d3dc80b65d69ca0bcbf05a0d8f36d71
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-18 00:07:17 +0000

    Created a reviews controller with actions for create and destroy.

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+
+  before_filter :authorize
+
+  def create
+    @product = Product.find(params['product_id'])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to :back
+    else
+      @reviews = @product.reviews.order(created_at: :desc)
+      @new_review = @review
+      render 'products/show', id: params['product_id']
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to :back
+  end
+
+  private
+
+    def review_params
+      params.require(:review).permit(
+        :description,
+        :rating,
+        :product_id
+      )
+    end
+end
