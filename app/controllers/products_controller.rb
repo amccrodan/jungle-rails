564468fde9d1bd7b1c7a6b8496d166f8b87ce011
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-18 00:03:40 +0000

    Added setup logic to display all reviews and a new review form.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,8 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
+    @reviews = @product.reviews.order(created_at: :desc)
+    @new_review = Review.new(product: @product)
   end
 
 end
