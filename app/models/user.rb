dc6e58de6fcc98870b57b6637eeac7db5e1ffa39
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-20 21:53:45 +0000

    Added class method User.authenticate_with_credentials and pulled some logic out of the sessions_controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,19 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 4 }
+  validates :password_confirmation, presence: true
 
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.where('lower(email) = ?', email.strip.downcase).first
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 
 end
