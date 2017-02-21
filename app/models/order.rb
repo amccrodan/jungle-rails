8ac9d63342e382d4a07a9cf28b813fa13d1a6964
Andrew McCrodan <andrewmccrodan@gmail.com>
2017-02-21 22:29:30 +0000

    Added a method that is called after order creation to reduce product quantities appropriately.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ActiveRecord::Base
 
+  after_create :update_quantities
+
   belongs_to :user
   has_many :line_items
 
@@ -7,4 +9,12 @@ class Order < ActiveRecord::Base
 
   validates :stripe_charge_id, presence: true
 
+  private
+    def update_quantities
+      self.line_items.each do |item|
+        product = Product.find(item.product_id)
+        product.quantity -= item.quantity
+        product.save!
+      end
+    end
 end
